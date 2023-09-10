4d8686f2876c899e07428e6880aa603ed07a2431
Jose Soares <joseserginho27@gmail.com>
2023-09-10 04:25:30 -0300

    Created: Position

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,11 +1,13 @@
 package aplicacao;
 
+import tabuleiro.Posicao;
+
 public class Programa {
 
 	public static void main(String[] args) {
-		System.out.println("Ola me chamo José."+ "\n"
-				+ "Caso esteja vendo esta mensagem quer dizer que você acessou meu repositório GitHub, fico feliz por isso, mas este projeto continua em andamento. Minha meta é terminar até o final de setembro de 2023 se tudo ocorrer bem.");
 		
+		Posicao posicao = new Posicao(3, 5);
+		System.out.println(posicao);
 		
 	}
 
