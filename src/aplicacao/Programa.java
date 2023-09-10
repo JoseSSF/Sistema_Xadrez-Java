b96754ece60b2358404a11db6f49bcaa6f61a3c3
Jose Soares <joseserginho27@gmail.com>
2023-09-10 03:45:35 -0300

    Projeto criado

---
+++ src/aplicacao/Programa.java
@@ -0,0 +1,12 @@
+package aplicacao;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		System.out.println("Ola me chamo José."+ "\n"
+				+ "Caso esteja vendo esta mensagem quer dizer que você acessou meu repositório GitHub, fico feliz por isso, mas este projeto continua em andamento. Minha meta é terminar até o final de setembro de 2023 se tudo ocorrer bem.");
+		
+		
+	}
+
+}
